a155dafd12fef3ef7128970e39870bb6a2e4676d
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-01-11 10:46:52 +0000

    Create vowcon.c

---
+++ vowcon.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+char n;
+scanf("%c",&n);
+if((n=='a'||n=='e'||n=='i'||n=='o'||n=='u')||(n=='A'||n=='E'||n=='I'||n=='O'||n=='U'))
+printf("char is vowel");
+else
+printf("CHAR IS CONSONANT");
+}
