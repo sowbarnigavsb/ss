5000d5922830df0f6f6e6dd50a09126b30f92718
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-01-21 13:24:27 +0530

    Create arm.c

---
+++ arm.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,c,r,s=0,m;
+	scanf("%d",&n);
+	m=n;
+	while(n!=0)
+	{
+		r=n%10;
+		c=r*r*r;
+		s+=c;
+		n=n/10;
+	}
+	if(s==m)
+	printf("Armstrong number");
+	else 
+	printf("no");
+	return 0;
